75bbe2c16104699e6f2b8255235e301cebfd00af
smurat <s.muratcelik@gmail.com>
2023-06-18 21:25:02 +0300

    Initial Commit

---
+++ variables.playground/Contents.swift
@@ -0,0 +1,29 @@
+import Foundation
+
+let myName = "Murat"
+let yourName = "Foo"
+
+var names = [
+    myName,
+    yourName
+]
+
+names.append("Bae")
+names.append("Baz")
+
+
+let foo = "Foo"
+var foo2 = foo
+foo2 = "Foo2"
+foo
+foo2
+
+
+let moreNames = [
+    "Foo",
+    "Bar"
+]
+var copy = moreNames
+copy.append("Baz")
+moreNames
+copy
